a3345dde30f1bb981bdab1bd955f7676fb9e77a5
minsung <sbr04119@naver.com>
2023-11-27 00:56:40 +0900

    add team url, templates have to fix

--- baseball/team/urls.py
+++ baseball/team/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.views.generic import TemplateView, ListView, CreateView, DetailView
 from . import views
+from .models import *
+
+app_name = 'team'
 
 urlpatterns = [
-    #path('team/', views.list, name = 'list'),
-    path('test1/', views.test1, name='test1'),
+    path('', TemplateView.as_view(template_name='team/team_list.html'), name='list'),
+    path('list/', ListView.as_view(model=Team), name='team'),
+    path('detail/<pk>/', DetailView.as_view(model=Team), name = 'team_detail'),
+    path('add/', CreateView.as_view(model=Team, fields='__all__'), name='team_add'),
 ]
