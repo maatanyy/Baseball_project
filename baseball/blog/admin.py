7883b92a2f8dc8898625ffce5c357167ed52eb0c
minsung <sbr04119@naver.com>
2023-12-02 15:56:17 +0900

    update admin

--- baseball/blog/admin.py
+++ baseball/blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(Tag)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'body']
+    list_display_links = ['id', 'title']
+    ordering= ['title']
+    list_filter = ['tag']
+    search_fields = ['body']
+    list_per_page = 5
+    
+    fieldsets = (
+        ('기본정보', {'fields':('title','body')}),
+        ('기타정보', {'fields':('tag', 'ip')})
+    )
+
+admin.site.register(Post, PostAdmin)
